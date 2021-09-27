880c5d275cd38e9da84c6c3b8f7cf2ff28afb0dc
Prince Ndebele <princendex460@hotmail.com>
2021-09-28 01:03:55 +0200

    I was able to finish the assignment

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ $(".savetextarea").on("click",function(){
     localStorage.setItem(id,plan)
     console.log("on-clicking",id,plan)
 })
-
+var currentHours = moment().hour()
+$("#currentDay").text(moment())
 for(let i=9;i<=17;i++){
     var useentry = localStorage.getItem(i)
     $("#"+i).siblings("textarea").val(useentry)
+    if(currentHours > 1){
+        $("#"+i).siblings("textarea").addClass("bg-success")
+
+    }else if( i == currentHours){
+        $("#"+i).siblings("textarea").addClass("bg-teal")
+    }else{
+        $("#"+i).siblings("textarea").addClass("bg-info")
+    }
+
 }

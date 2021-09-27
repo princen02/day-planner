0ff317584d568a0c63313d413d89efe50e337c55
Prince Ndebele <princendex460@hotmail.com>
2021-09-28 00:52:12 +0200

    I was able to save text to local storage

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ console.log("JS loaded")
 $(".savetextarea").on("click",function(){
     var id= $(this).attr("id")
     var plan = $(this).siblings("textarea").val()
+    localStorage.setItem(id,plan)
     console.log("on-clicking",id,plan)
 })
